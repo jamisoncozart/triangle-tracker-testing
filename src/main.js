e803436cd9fcc345338eef51d80bf613f922c078
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-03 19:46:42 -0800

    Add html-webpack-plugin, add Triangle constructor, add basic input storing at from submit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { Triangle } from "./triangle.js";
+
+$(document).ready(function() {
+  $("#triangleForm").submit(function(event) {
+    event.preventDefault();
+    var side1 = $("#side1").val();
+    var side2 = $("#side2").val();
+    var side3 = $("#side3").val();
+    var triangle = new Triangle(side1, side2, side3);
+    console.log(triangle);  
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Triangle',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ]
+};
